05c441d48b4f1e68d13289e5a96945cb04388d04
Vladimir Gasic <vgashic@gmail.com>
2016-08-02 02:14:50 +0200

    Changed to bootstrap 3.3.7

--- js/custom.js
+++ js/custom.js
@@ -6,28 +6,34 @@ var channelUrl = "https://api.twitch.tv/kraken/channels/";
 
 function fillStreamersList(name) {
 
-	$.getJSON(channelUrl + name, function(data) {
-		var logoUrl = data.logo;
-		var profileUrl = data.url;
+	$.getJSON(channelUrl + name, function(channelData) {
+		var logoUrl = channelData.logo;
+		var profileUrl = channelData.url;
 
-		$.getJSON(streamUrl + name, function(data) {
-			console.log(logoUrl);
+
+		$.getJSON(streamUrl + name, function(streamData) {
+			console.log(streamData);
+
+			var game = streamData.stream === null ? "Offline" : channelData.game;
+			var status = streamData.stream === null ? "" : channelData.status;
+
 			var htmlElement = "<div class='row channel {status}'>\n \
-	<div class='col-sm-1'>\n \
+	<div class='col-xs-2 channel-logo'>\n \
 		<img src='" + logoUrl + "' class='logo img-circle'>\n \
 	</div>\n \
-		<div class='col-sm-3'>\n \
-			<a href='" + profileUrl + "'>" + name + "</a> \n \
-		</div>\n \
-		<div class='col-sm-8'>\n \
-			<span>status</span> \n \
-		</div>\n \
+	<div class='col-xs-10 col-sm-3 channel-name'>\n \
+		<a href='" + profileUrl + "'>" + name + "</a> \n \
+	</div>\n \
+	<div class='col-xs-10 col-sm-7 channel-data'>\n \
+		<span>" + game + "</span> \n \
+		<span class='hidden-xs-down'>" + status + "</span> \n \
+	</div>\n \
 	</div>\n \
 </div>";
 
 
 
-			if (data.stream === null) {
+			if (streamData.stream === null) {
 				$(".streamers-list-offline").append(htmlElement.replace("{status}", "offline"));
 			} else {
 				$(".streamers-list-online").append(htmlElement.replace("{status}", "online"));
@@ -39,6 +45,8 @@ function fillStreamersList(name) {
 }
 
 function getStreamersData(arr) {
+	"use strict";
+	console.log(arr);
 	for (var i = 0; i < arr.length; i++) {
 		fillStreamersList(arr[i]);
 	}
